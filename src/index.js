de2408865d6da96e384f5deeac1862152e554ee1
Jan Wehner <jan.wehner@securepoint.de>
2021-04-26 10:18:26 +0200

    remove install func

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import VueLadda from './vue-ladda.vue';
 
-const install = (app, options) => {
-    app.component('vue-ladda', VueLadda);
-};
-
-VueLadda.install = install;
-
 export default VueLadda;
-
-//export { VueLadda };
